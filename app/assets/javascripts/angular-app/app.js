216dbd094f9069cfc86fd5013202f998f010400c
Joshua Richardson <joshrichardson1234@hotmail.com>
2014-12-15 17:24:03 +1100

    UI router working

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -7,17 +7,31 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
       .state('home', {
         url: '/',
         templateUrl: '/assets/angular-app/partials/home.html',
-        controller: 'AboutContrl'
+        controller: 'MedSessionCtrl'
       })
       .state('about', {
         url: '/about',
         templateUrl: '/assets/angular-app/partials/about.html',
         controller: 'AboutContrl'
       })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: '/assets/angular-app/partials/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .state('guide', {
+        url: '/guide',
+        templateUrl: '/assets/angular-app/partials/guide.html',
+        controller: 'MedOptionsCtrl'
+      })
+      .state('music', {
+        url: '/music',
+        templateUrl: '/assets/angular-app/partials/music.html',
+        controller: 'MedOptionsCtrl'
+      })
 
     // default fall back route
     $urlRouterProvider.otherwise('/');
-
 
 });
 
