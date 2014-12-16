f572aac3daad4cd3c98604bfe1006a365e9e36d3
Joshua Richardson <joshrichardson1234@hotmail.com>
2014-12-16 15:26:46 +1100

    Workinggggg

--- app/assets/javascripts/angular-app/controllers/meditationSession.js
+++ app/assets/javascripts/angular-app/controllers/meditationSession.js
@@ -4,18 +4,23 @@ app.controller('MedSessionCtrl', ['$scope', 'MeditationSessionServ', function($s
   console.log("Music on is: " + MeditationSessionServ.getMusic());
   console.log("Time is: " + MeditationSessionServ.getTime());
 
-  if ()
+  if (MeditationSessionServ.getGuide() && MeditationSessionServ.getMusic()) {
+    console.log("music and voice on");
+  };
 
   $scope.playing = false;
   $scope.audio = document.createElement('audio');
+  $scope.audio2 = document.createElement('audio');
   $scope.audio.src = 'http://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg';
-  $scope.audio.src = 'http://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg';
+  $scope.audio2.src = 'https://s3.amazonaws.com/uploads.hipchat.com/39979/1250606/xIoFUbbxvxQnWYm/01%20Love%20%26%20Devotion.mp3';
   $scope.play = function() {
     $scope.audio.play();
+    $scope.audio2.play();
     $scope.playing = true;
   };
   $scope.stop = function() {
     $scope.audio.pause();
+    $scope.audio2.pause();
     $scope.playing = false;
   };
   $scope.audio.addEventListener('ended', function() {
