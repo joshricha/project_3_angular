cff76e4aca85deaccf2838994317bd3999b509e3
Joshua Richardson <joshrichardson1234@hotmail.com>
2014-12-17 12:24:56 +1100

    if else refactoring but I just thought of a better way but leave until later

--- app/assets/javascripts/angular-app/controllers/meditationSession.js
+++ app/assets/javascripts/angular-app/controllers/meditationSession.js
@@ -1,5 +1,6 @@
 app.controller('MedSessionCtrl', ['$scope', 'MeditationSessionServ', function($scope, MeditationSessionServ){
 
+  // for testing don't delete
   window.medservice = MeditationSessionServ
 
   console.log("Guide on is: " + MeditationSessionServ.getGuide());
@@ -9,8 +10,45 @@ app.controller('MedSessionCtrl', ['$scope', 'MeditationSessionServ', function($s
 
   var initMeditation = function(){
 
-    // checks which track to play according to the chosen options
+    // refactoring the if else. Should put this in a function which takes params such as voiceOn, music on, length etc then provides the mp3 for those params
 
+    // if (MeditationSessionServ.getTime() ==  10)) {
+    //   if (MeditationSessionServ.getGuide()) {
+    //     // 10 minute timer and guide on
+    //     if (MeditationSessionServ.getMusic()) {
+    //       // 10 min timer guide on and music on
+    //     } else {
+    //       // 10 min timer guide on and music off
+    //     };
+    //   } else {
+    //     // 10 minute timer and guide off
+    //       if (MeditationSessionServ.getMusic()) {
+    //       // 10 minute timer and guide off music off
+    //     } else {
+    //        // 10 minute timer and guide off music on
+    //     }
+    //   }
+
+    // } else if (MeditationSessionServ.getTime() ==  20)) {
+    //     if (MeditationSessionServ.getGuide()) {
+    //     // 20 minute timer and guide on
+    //       if (MeditationSessionServ.getMusic()) {
+    //       // 20 min timer guide on and music on
+    //       } else {
+    //       // 20 min timer guide on and music off
+    //       };
+    //     } else {
+    //       // 20 minute timer and guide off
+    //       if (MeditationSessionServ.getMusic()) {
+    //       // 20 minute timer and guide off music off
+    //     } else {
+    //        // 20 minute timer and guide off music on
+    //     }
+    //   }
+    // }
+
+
+    // checks which track to play according to the chosen options
     if (MeditationSessionServ.getGuide() && MeditationSessionServ.getMusic() && (MeditationSessionServ.getTime() ==  10)) {
       console.log("music and voice on and 10 mins");
       playTrack('http://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg')
@@ -37,6 +75,14 @@ app.controller('MedSessionCtrl', ['$scope', 'MeditationSessionServ', function($s
       console.log("not chosen.")
     };
 
+  };
+
+  var quickStart = function(time){
+    if (time == 10) {
+      console.log("starting quick 10")
+    } else {
+      console.log("starting quick 20")
+    };
   };
 
   var myFunction = function(){
