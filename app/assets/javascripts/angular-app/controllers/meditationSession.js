37c2547b1ca1573b8edcda1fa24fbc8f53f6c9d3
Joshua Richardson <joshrichardson1234@hotmail.com>
2014-12-18 16:22:09 +1100

    Meditations working

--- app/assets/javascripts/angular-app/controllers/meditationSession.js
+++ app/assets/javascripts/angular-app/controllers/meditationSession.js
@@ -12,40 +12,49 @@ app.controller('MedSessionCtrl', ['$scope', '$http', 'MeditationSessionServ', fu
 
     // quick starts
     if (MeditationSessionServ.getQuickStart() == 10) {
-      playTrack('http://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg');
+      playTrack('https://s3.amazonaws.com/uploads.hipchat.com/39979/1250606/NruhUzs05rmkqLJ/20%20minute%20Guided%20Mindfulness%20Exercise.mp3');
       $scope.time = 10;
 
     } else if (MeditationSessionServ.getQuickStart() == 20) {
-      console.log("starting 20")
-
+      playTrack('https://s3.amazonaws.com/uploads.hipchat.com/39979/1250606/NruhUzs05rmkqLJ/20%20minute%20Guided%20Mindfulness%20Exercise.mp3');
+      $scope.time = 20;
     };
 
     // Should put this in a function which takes params such as voiceOn, music on, length etc then provides the mp3 for those params
 
     // checks which track to play according to the chosen options
     if (MeditationSessionServ.getGuide() && MeditationSessionServ.getMusic() && (MeditationSessionServ.getTime() ==  10)) {
-      console.log("music and voice on and 10 mins");
-      playTrack('http://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg');
+      // playTrack('http://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg');
+      playTrack('https://s3.amazonaws.com/uploads.hipchat.com/39979/1250606/kxuxNl335K00g56/10%20Minute%20TRANSCENDENT%20GUIDED%20MEDITATION.mp3');
       $scope.time = 10;
 
     } else if (MeditationSessionServ.getGuide() && MeditationSessionServ.getMusic() && (MeditationSessionServ.getTime() ==  20)) {
-      console.log("Guide on, voice on - 20mins")
+      playTrack('https://s3.amazonaws.com/uploads.hipchat.com/39979/1250606/jlEN1Gw4uSsN6SK/Guided%20Meditation%20-%20Blissful%20Deep%20Relaxation.mp3');
+      $scope.time = 20;
 
     } else if (MeditationSessionServ.getGuide() == false && MeditationSessionServ.getMusic() && (MeditationSessionServ.getTime() ==  10)) {
-      console.log("guide off, music on, time 10")
+      playTrack('https://s3.amazonaws.com/uploads.hipchat.com/39979/1250606/ycAhj7gYB5AmsLV/10%20Minutes%20Meditation%20Music%20Ambient%20%28Deep%20Meditation%2010%20Min%20mysoftmusic%29.mp3');
+      $scope.time = 10;
 
     } else if (MeditationSessionServ.getGuide() == false && MeditationSessionServ.getMusic() && (MeditationSessionServ.getTime() ==  20)) {
-      console.log("guide off, music on, time 20")
+      playTrack('https://s3.amazonaws.com/uploads.hipchat.com/39979/1250606/k5sB1jQ8Q6OCBrZ/Meditation%20and%20Healing%20Music%20-%2020%20min%20of%20Deep%20Meditation%20-%20Clouds%2C%20Peace%20and%20Serenity.mp3');
+      $scope.time = 20;
 
     } else if (MeditationSessionServ.getGuide() && MeditationSessionServ.getMusic() == false && MeditationSessionServ.getTime() ==  10) {
-      console.log("guide on, music off, time 10")
+      playTrack('https://s3.amazonaws.com/uploads.hipchat.com/39979/1250606/k5sB1jQ8Q6OCBrZ/Meditation%20and%20Healing%20Music%20-%2020%20min%20of%20Deep%20Meditation%20-%20Clouds%2C%20Peace%20and%20Serenity.mp3');
+      $scope.time = 10;
 
     } else if (MeditationSessionServ.getGuide() && MeditationSessionServ.getMusic() == false && (MeditationSessionServ.getTime() ==  20)) {
-      console.log("guide on, music off, time 20")
+      playTrack('https://s3.amazonaws.com/uploads.hipchat.com/39979/1250606/NruhUzs05rmkqLJ/20%20minute%20Guided%20Mindfulness%20Exercise.mp3');
+       $scope.time = 10;
+
     } else if (MeditationSessionServ.getGuide() == false && MeditationSessionServ.getMusic() == false && (MeditationSessionServ.getTime() ==  10)) {
-      console.log("guide on, music off, time 20")
+      $scope.time = 20;
+
     } else if (MeditationSessionServ.getGuide() == false && MeditationSessionServ.getMusic() == false && (MeditationSessionServ.getTime() ==  20)) {
       console.log("guide on, music off, time 20")
+      $scope.time = 20;
+
     } else {
       console.log("not chosen.")
     };
