0c0b8b191aa4d9cc6f7dd55db44a1760c6c68308
Joshua Richardson <joshrichardson1234@hotmail.com>
2014-12-18 19:31:36 +1100

    Buttons and charts

--- app/assets/javascripts/angular-app/controllers/statistic.js
+++ app/assets/javascripts/angular-app/controllers/statistic.js
@@ -31,5 +31,20 @@ app.controller('StatisticCtrl', function($http, $scope){
         return data;
       });
   };
+
+
+  var genChart = function() {
+    var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    }
+    });
+  }
+
+genChart();
   getUserId();
 })
